86b71b4fa97717b36588b74c8df1289447babc41
ATMANI <63373757+ayoubatmani@users.noreply.github.com>
2020-05-25 17:07:16 +0200

    Create addusr.sh

---
+++ miscellaneous/addusr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script allows the root user or admin to add new users 
+# to the system in an easier way by just typing the user name and password (The password is entered in an encrypted manner).
+if [ $(id -u) -eq 0 ]; then
+    read -p "Enter username : " username
+    read -s -p "Enter password : " password
+    egrep "^$username" /etc/passwd >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$username exists!"
+        exit 1
+    else
+        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+        useradd -m -p $pass $username
+        [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+    fi
+else
+    echo "Only root may add a user to the system"
+    exit 2
+fi
